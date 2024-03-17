73c59ad623968597c8ff2f5045864a50d65ec9c6
iammithun <157653522+iammithun@users.noreply.github.com>
2024-03-17 18:54:04 -0400

    Add files via upload

---
+++ light_speed.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 17 18:48:43 2024
+
+@author: iamrs
+"""
+
+from scipy.constants import c
+
+# Print the speed of light in meters per second
+print("Speed of light:", c, "m/s")
